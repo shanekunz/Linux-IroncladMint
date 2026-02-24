c2777bd6e3f07fbfefcc7d28846cf1ea9992cdc5
shane kunz <shanemkunz@gmail.com>
2026-02-24 09:53:56 -0600

    Refine Vice Dusk palette and statusline contrast

--- nvim/.config/nvim/lua/plugins/theme-vice-dusk.lua
+++ nvim/.config/nvim/lua/plugins/theme-vice-dusk.lua
@@ -20,9 +20,9 @@ return {
 
         c.comment = "#8e6db1"
 
-        c.blue = "#67d9f8"
-        c.cyan = "#50d5ff"
-        c.green = "#8ef3b3"
+        c.blue = "#9a8cff"
+        c.cyan = "#b07cff"
+        c.green = "#d6b86a"
         c.magenta = "#d782ff"
         c.purple = "#b96dff"
         c.yellow = "#ffce73"
@@ -57,8 +57,8 @@ return {
 
         hl.DiagnosticVirtualTextError = { fg = "#ff97c8", bg = "#331325" }
         hl.DiagnosticVirtualTextWarn = { fg = "#ffd48a", bg = "#35250e" }
-        hl.DiagnosticVirtualTextInfo = { fg = "#9defff", bg = "#122635" }
-        hl.DiagnosticVirtualTextHint = { fg = "#98f9ce", bg = "#153126" }
+        hl.DiagnosticVirtualTextInfo = { fg = "#d5c7ff", bg = "#241538" }
+        hl.DiagnosticVirtualTextHint = { fg = "#f0cd87", bg = "#362612" }
       end
     end,
   },
@@ -67,5 +67,56 @@ return {
     opts = {
       colorscheme = "tokyonight",
     },
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      local vice = {
+        bg_base = "#2a1530",
+        bg_mid = "#3a1f45",
+        bg_strong = "#4c265c",
+        fg = "#f4ecff",
+        fg_muted = "#d7c1f4",
+        orange = "#ff9f1c",
+        purple = "#b96dff",
+        magenta = "#d782ff",
+        pink = "#ff6cae",
+        amber = "#ffb347",
+      }
+
+      opts.options = opts.options or {}
+      opts.options.theme = {
+        normal = {
+          a = { fg = "#1a0f1f", bg = vice.orange, bold = true },
+          b = { fg = vice.fg, bg = vice.bg_strong },
+          c = { fg = vice.fg_muted, bg = vice.bg_mid },
+        },
+        insert = {
+          a = { fg = "#1a0f1f", bg = vice.purple, bold = true },
+          b = { fg = vice.fg, bg = vice.bg_strong },
+          c = { fg = vice.fg_muted, bg = vice.bg_mid },
+        },
+        visual = {
+          a = { fg = "#1a0f1f", bg = vice.magenta, bold = true },
+          b = { fg = vice.fg, bg = vice.bg_strong },
+          c = { fg = vice.fg_muted, bg = vice.bg_mid },
+        },
+        replace = {
+          a = { fg = "#1a0f1f", bg = vice.pink, bold = true },
+          b = { fg = vice.fg, bg = vice.bg_strong },
+          c = { fg = vice.fg_muted, bg = vice.bg_mid },
+        },
+        command = {
+          a = { fg = "#1a0f1f", bg = vice.amber, bold = true },
+          b = { fg = vice.fg, bg = vice.bg_strong },
+          c = { fg = vice.fg_muted, bg = vice.bg_mid },
+        },
+        inactive = {
+          a = { fg = vice.fg_muted, bg = vice.bg_base },
+          b = { fg = vice.fg_muted, bg = vice.bg_base },
+          c = { fg = vice.fg_muted, bg = vice.bg_base },
+        },
+      }
+    end,
   },
 }
